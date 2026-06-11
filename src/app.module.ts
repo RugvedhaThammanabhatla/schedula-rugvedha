b3b21671308aa25d3e9deab64b8317ef1700c86d
Rugvedha Thammanabhatla <missrugvedha@gmail.com>
2026-06-11 18:26:56 +0530

    Day 5 Backend Deployment

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,20 +1,34 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+
 import { AuthModule } from './auth/auth.module';
 import { DoctorModule } from './doctor/doctor.module';
 import { PatientModule } from './patient/patient.module';
 
 @Module({
   imports: [
-  ConfigModule.forRoot({
-    isGlobal: true,
-  }),
-  AuthModule,
-  DoctorModule,
-  PatientModule,
-],
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      url: process.env.DATABASE_URL,
+      autoLoadEntities: true,
+      synchronize: true,
+      ssl: {
+        rejectUnauthorized: false,
+      },
+    }),
+
+    AuthModule,
+    DoctorModule,
+    PatientModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
